48296e1f992f0e5db7b2013de4482822176290a2
The SMEXY Daniel Dale <sexybeast@localhost.localdomain>
2017-08-19 20:19:44 +0100

    Added names to files

--- charactercreator.cpp
+++ charactercreator.cpp
@@ -45,15 +45,14 @@ bool CharacterCreator::populateNamesArray()
     //initialise and open first and second input streams
     std::ifstream firstNameStream;
     std::ifstream secondNameStream;
-    firstNameStream.open("/home/dev/NetBeansProjects/TestMentalBreakSys/Names/firstNames.txt", std::ifstream::in);
-    secondNameStream.open("/home/dev/NetBeansProjects/TestMentalBreakSys/Names/secondNames.txt", std::ifstream::in);
+    firstNameStream.open("./Names/firstNames.txt", std::ifstream::in);
+    secondNameStream.open("./Names/secondNames.txt", std::ifstream::in);
 
     //check if streams opened
     if( !firstNameStream.is_open() )
     {
         std::cout<< "First Names File not opened!" <<std::endl;
         possibleFirstNames.push_back("Couldn't connect");
-        return false;
     }
     else
     {
@@ -63,7 +62,7 @@ bool CharacterCreator::populateNamesArray()
     if( !secondNameStream.is_open() )
     {
         std::cout<< "Second Names File not opened!" <<std::endl;
-        possibleFirstNames.push_back("Couldn't connect");
+        possibleSurnames.push_back("Couldn't connect");
         return false;
     }
     else
